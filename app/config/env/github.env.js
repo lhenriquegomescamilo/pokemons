f6459d5a07442fee679c32f8902c520a9749417a
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2018-07-12 23:48:14 -0300

    master: configure travis MYSQL

---
+++ app/config/env/github.env.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: 'development',
+    secret: 'S3cr3t',
+    serverPort: 3000,
+    dialect: 'mysql',
+    database: 'challenge-jazida',
+    username: 'root',
+    password: 'challenge-jazida',
+    port: 3306,
+    dbURL: 'localhost',
+    host: 'localhost',
+    define: {
+        // prevent sequelize from pluralizing table names
+        freezeTableName: true,
+
+        // don't delete database entries but set the newly added attribute deletedAt
+        // to the current date (when deletion was done). paranoid will only work if
+        // timestamps are enabled
+        // paranoid: true,
+
+        // don't use camelcase for automatically added attributes but underscore style
+        // so updatedAt will be updated_at
+        underscored: true
+    }
+};
